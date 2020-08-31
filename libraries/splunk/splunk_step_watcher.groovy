2706e5f5ec2eed89dd27911993e314586ccdec33
MADELA Patrick <patrick.madela@synchrotron-soleil.fr>
2020-08-31 14:56:03 +0200

    Restricting Hook Execution

--- libraries/splunk/splunk_step_watcher.groovy
+++ libraries/splunk/splunk_step_watcher.groovy
@@ -3,7 +3,4 @@ void before(context){
     println "Splunk: running before the ${context.library} library's ${context.step} step"
 }
 
-@AfterStep
-void after(context){
-    println "Splunk: running after the ${context.library} library's ${context.step} step"
-}
+@AfterStep({ context.step.equals("static_code_analysis") })
